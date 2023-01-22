e17f07b2a2ac7a62fdec840d4b072054d6b136fb
Mark Nosal <mark.c.nosal@gmail.com>
2023-01-22 13:26:52 -0600

    Completed another pretty intense lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driverNames, string) {
+    return driverNames.filter((blob) => blob.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch (myArray, letters) {
+    return myArray.filter((blob) => blob.toUpperCase().indexOf(letters.toUpperCase()) === 0)
+}
+
+function matchName (finalArray, lastString) {
+    return finalArray.filter((element) => element['name'] === lastString);
+}
